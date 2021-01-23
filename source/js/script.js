507d962bc18a5df1ed471adb4f548b0dfcc367b5
Aleksandr Alekseev <hoblex@gmail.com>
2021-01-23 20:19:03 +0300

    убраны двойные и тройные пробелы между атрибутами. убраны куски закомментированного кода в стилях

--- source/js/script.js
+++ source/js/script.js
@@ -15,61 +15,23 @@ var navToggle = document.querySelector('.header-nav__toggle');
 }
 });
 
-//Обработка событий слайдера
-// const sliderNext = document.querySelector('.slider-nav__button--next');
-// const sliderPrev = document.querySelector('.slider-nav__button--prev');
-//
-// const sliderItems = document.querySelectorAll('.slider__item');
-//
-// sliderNext.onclick = function () {
-//   if (sliderItems[0].classList.contains('slider__item--current')) {
-//     sliderItems[0].classList.remove('slider__item--current')
-//     sliderItems[1].classList.add('slider__item--current')
-//     sliderItems[2].classList.remove('slider__item--current')
-//   } else if (sliderItems[1].classList.contains('slider__item--current')) {
-//     sliderItems[0].classList.remove('slider__item--current')
-//     sliderItems[1].classList.remove('slider__item--current')
-//     sliderItems[2].classList.add('slider__item--current')
-//   } else {
-//     sliderItems[0].classList.add('slider__item--current')
-//     sliderItems[1].classList.remove('slider__item--current')
-//     sliderItems[2].classList.remove('slider__item--current')
-//   }
-// };
-//
-// sliderPrev.onclick = function () {
-//   if (sliderItems[0].classList.contains('slider__item--current')) {
-//     sliderItems[0].classList.remove('slider__item--current')
-//     sliderItems[1].classList.remove('slider__item--current')
-//     sliderItems[2].classList.add('slider__item--current')
-//   } else if (sliderItems[1].classList.contains('slider__item--current')) {
-//     sliderItems[0].classList.add('slider__item--current')
-//     sliderItems[1].classList.remove('slider__item--current')
-//     sliderItems[2].classList.remove('slider__item--current')
-//   } else {
-//     sliderItems[0].classList.remove('slider__item--current')
-//     sliderItems[1].classList.add('slider__item--current')
-//     sliderItems[2].classList.remove('slider__item--current')
-//   }
-// };
-
 //Обработка событий модального окна "Заказ товара дня"
 
 var buttonProductToBuy = document.querySelectorAll('.form-to-order');
 var displayModalProductToBuy = document.querySelector('.modal-cart');
 
 for (var i = 0; i < buttonProductToBuy.length; i++) {
-  buttonProductToBuy[i].addEventListener("click", function (evt) {
+  buttonProductToBuy[i].addEventListener('click', function (evt) {
     evt.preventDefault();
-    displayModalProductToBuy.classList.add("modal-cart--show");
+    displayModalProductToBuy.classList.add('modal-cart--show');
   });
 }
 
-window.addEventListener("keydown", function (evt) {
+window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
-    if (displayModalProductToBuy.classList.contains("modal-cart--show")) {
+    if (displayModalProductToBuy.classList.contains('modal-cart--show')) {
       evt.preventDefault();
-      displayModalProductToBuy.classList.remove("modal-cart--show");
+      displayModalProductToBuy.classList.remove('modal-cart--show');
     }
   }
 });
